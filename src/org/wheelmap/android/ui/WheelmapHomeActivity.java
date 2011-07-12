717424aa4af802aa0ab3af58fa9d30c39d358142
Michal Harakal <michal.harakal@googlemail.com>
2011-07-13 01:55:44 +0200

    home screan according google design patterns as dasboard pattern

---
+++ src/org/wheelmap/android/ui/WheelmapHomeActivity.java
@@ -0,0 +1,13 @@
+package org.wheelmap.android.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WheelmapHomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
