63b2e7f07ef7e928a31625d545b8a5c89b70c50a
Michal Harakal <michal.harakal@googlemail.com>
2011-07-13 08:48:14 +0200

    initial version of POIsListView

---
+++ src/org/wheelmap/android/ui/POIsListActivity.java
@@ -0,0 +1,15 @@
+package org.wheelmap.android.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class POIsListActivity extends Activity {
+	
+	 /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+
+}
--- src/org/wheelmap/android/ui/WheelmapHomeActivity.java
+++ src/org/wheelmap/android/ui/WheelmapHomeActivity.java
@@ -1,7 +1,11 @@
 package org.wheelmap.android.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import org.wheelmap.android.ui.POIsListActivity;
 
 public class WheelmapHomeActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,17 @@ public class WheelmapHomeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    
+    /** Handle "schedule" action. */
+    public void onListClick(View v) {
+        // Launch overall conference schedule
+        startActivity(new Intent(this, POIsListActivity.class));
+    }
+
+    /** Handle "map" action. */
+    public void onMapClick(View v) {
+        // Launch map of conference venue
+        //startActivity(new Intent(this, MapActivity.class));
     }
 }
