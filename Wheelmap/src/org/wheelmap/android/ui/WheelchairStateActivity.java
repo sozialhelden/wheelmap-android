8ae7b4c81bce30526b053cb2d8c0758b8059ea61
Michal Harakal <michal.harakal@googlemail.com>
2011-08-28 02:40:32 +0200

    WheelchairStateActivity

---
+++ Wheelmap/src/org/wheelmap/android/ui/WheelchairStateActivity.java
@@ -0,0 +1,17 @@
+package org.wheelmap.android.ui;
+
+import org.wheelmap.android.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WheelchairStateActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_wheelchair_state);
+	}
+	
+
+}
