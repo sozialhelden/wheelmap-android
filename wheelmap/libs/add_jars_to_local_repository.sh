bc5058a32f7fb53546cea29cb0ce4dc864fcb8e9
RR <Rutton@web.de>
2012-05-26 03:03:48 +0200

    - fixed pulltorefresh library

--- wheelmap/libs/add_jars_to_local_repository.sh
+++ wheelmap/libs/add_jars_to_local_repository.sh
@@ -2,5 +2,5 @@ mvn install:install-file -Dfile=libGoogleAnalytics.jar -DgroupId=org.wheelmap.an
     -DartifactId=googleanalytics -Dversion=1.0-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=mapsforge-map-0.2.4.jar -DgroupId=org.wheelmap.android \
     -DartifactId=mapsforge-map -Dversion=0.2.4 -Dpackaging=jar
-mvn install:install-file -Dfile=pulltorefresh-library-1.2.8.jar -DgroupId=org.wheelmap.android \
-    -DartifactId=pulltorefresh-library -Dversion=1.2.8 -Dpackaging=jar
+mvn install:install-file -Dfile=pulltorefresh-library-1.2.8.apklib -DgroupId=com.github.handmark.pulltorefresh \
+    -DartifactId=pulltorefresh-library -Dversion=1.2.8 -Dpackaging=apklib
