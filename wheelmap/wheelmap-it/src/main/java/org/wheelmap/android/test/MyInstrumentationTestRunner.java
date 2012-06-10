f0ae19d99ca0d4ae3ae1fc9e85184cef15effc3f
RR <Rutton@web.de>
2012-06-10 02:13:44 +0200

    - all files with shiny new copyright header created by maven plugin   old copyright header removed

--- wheelmap/wheelmap-it/src/main/java/org/wheelmap/android/test/MyInstrumentationTestRunner.java
+++ wheelmap/wheelmap-it/src/main/java/org/wheelmap/android/test/MyInstrumentationTestRunner.java
@@ -1,20 +1,24 @@
 /*
-Copyright (C) 2011 Michal Harakal and Michael Kroez
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS-IS" BASIS
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-        
-*/
-
+ * #%L
+ * Wheelmap-it - Integration tests
+ * %%
+ * Copyright (C) 2011 - 2012 Michal Harakal - Michael Kroez - Sozialhelden e.V.
+ * %%
+ * Wheelmap App based on the Wheelmap Service by Sozialhelden e.V.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package org.wheelmap.android.test;
 
 import junit.framework.TestSuite;
