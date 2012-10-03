de98fb7e87a701c45e5cdb036ce7c953e4777dcf
RR <rutton.r@gmail.com>
2012-10-04 00:50:37 +0200

    - fixed login testing

--- wheelmap/wheelmap-it/src/main/java/org/wheelmap/android/test/LoginTest.java
+++ wheelmap/wheelmap-it/src/main/java/org/wheelmap/android/test/LoginTest.java
@@ -24,16 +24,16 @@ package org.wheelmap.android.test;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.regex.Pattern;
+
+import junit.framework.Assert;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.util.UriUtils;
 import org.wheelmap.android.service.SyncServiceException;
 
-
 import wheelmap.org.domain.apikey.AuthInfo;
-import wheelmap.org.domain.apikey.User;
 import wheelmap.org.request.AcceptType;
 import wheelmap.org.request.ApiKeyRequestBuilder;
 import wheelmap.org.request.RequestProcessor;
@@ -41,43 +41,71 @@ import android.test.AndroidTestCase;
 import android.util.Log;
 
 public class LoginTest extends AndroidTestCase {
-	private final static String TAG = "logintest";	
+	private final static String TAG = "logintest";
 	private String server = "staging.wheelmap.org";
-	
+
 	private int statusAuthFailed = 400;
 	private int statusOSMFailed = 403;
-	
+
 	public void testLoginTest() throws URISyntaxException {
-		
-		ApiKeyRequestBuilder rb = new ApiKeyRequestBuilder(server, AcceptType.JSON );
-		// rb.setCredentials( "olfila@gmx.net", "testtest" ); // correct
-		rb.setCredentials( "olfila@gmx.ne", "testtest" ); // wrong
-		rb.setCredentials( "rutton@web.de", "testtest" ); // no osm connection
-		
+
+		ApiKeyRequestBuilder rb = new ApiKeyRequestBuilder(server,
+				AcceptType.JSON);
+		rb.setCredentials("olfila@gmx.ne", "testtest"); // wrong
+		int result = testRequest(rb);
+		Assert.assertEquals(result, 2);
+
+		rb.setCredentials("rutton@web.de", "testtest"); // no osm connection
+		result = testRequest(rb);
+		Assert.assertEquals(result, 1);
+
+		rb.setCredentials("rutton.r@gmail.com", "testtest"); // correct
+		result = testRequest(rb);
+		Assert.assertEquals(result, 0);
+
+	}
+
+	private int testRequest(ApiKeyRequestBuilder rb) {
+
 		String request;
 		try {
-			request = UriUtils.encodeQuery(rb.buildRequestUri(),
-					"utf-8");
+			request = UriUtils.encodeQuery(rb.buildRequestUri(), "utf-8");
 		} catch (UnsupportedEncodingException e) {
-			throw new SyncServiceException( SyncServiceException.ERROR_INTERNAL_ERROR, e);
+			throw new SyncServiceException(
+					SyncServiceException.ERROR_INTERNAL_ERROR, e);
 		}
-		
-		Log.d( TAG, "Request = " + request );
+
+		Log.d(TAG, "Request = " + request);
 
 		RequestProcessor rp = new RequestProcessor();
 		AuthInfo authInfo = null;
 		try {
-			authInfo = rp.post( new URI(request), null, AuthInfo.class );
-		} catch ( HttpClientErrorException e ) {
+			authInfo = rp.post(new URI(request), null, AuthInfo.class);
+		} catch (HttpClientErrorException e) {
 			HttpStatus status = e.getStatusCode();
-			if ( status.value() == statusAuthFailed )
-				Log.d( TAG, "wrong email or password");
-			else if (status.value() == statusOSMFailed )
-				Log.d( TAG, "not osm connected" );
+			if (status.value() == statusAuthFailed) {
+				Log.d(TAG, "wrong email or password");
+				return 2;
+			} else if (status.value() == statusOSMFailed) {
+				Log.d(TAG, "not osm connected");
+				return 1;
+			}
+
+		} catch (RestClientException e) {
+			e.printStackTrace();
+			Assert.fail("Login failed");
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			Assert.fail("Login failed");
 		}
-				
-		if ( authInfo != null )		
-			Log.d( TAG, "User: olfila@gmx.net " + " api_key = " + authInfo.getUser().getApiKey());
-		
+
+		if (authInfo != null)
+			Log.d(TAG, "User: olfila@gmx.net " + " api_key = "
+					+ authInfo.getUser().getApiKey());
+		else
+			Assert.fail("Login failed");
+
+		return 0;
+
 	}
 }
