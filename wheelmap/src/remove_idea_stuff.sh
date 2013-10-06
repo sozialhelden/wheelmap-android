071cfc120aa536752cdcfad8e8bd579580bff396
RR <rutton.r@gmail.com>
2013-10-06 04:44:24 +0200

    added some verbosity to idea clean script

--- wheelmap/src/remove_idea_stuff.sh
+++ wheelmap/src/remove_idea_stuff.sh
@@ -1,6 +1,14 @@
 TARGETDIR=../..
 
-rm -rf $TARGETDIR/out
-rm -rf $TARGETDIR/.idea
-find $TARGETDIR -name \*.iml|xargs rm -rf
-find $TARGETDIR -name gen-\*|xargs rm -rf
+checkIfAvailableAndRemove() {
+
+  if [ -d $1 ]; then
+    echo "Deleting $1"
+    rm -rfv $1;
+  fi
+}
+
+checkIfAvailableAndRemove $TARGETDIR/out
+checkIfAvailableAndRemove TARGETDIR/.idea
+find $TARGETDIR -name \*.iml|xargs rm -rfv
+find $TARGETDIR -name gen-\*|xargs rm -rfv
