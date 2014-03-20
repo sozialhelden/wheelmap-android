44ca17f6025f1a437a7af2beb10c8b7afe169579
Alexandra Fehkührer <alexandra.fehkuehrer@smartmobilefactory.com>
2014-03-20 15:58:55 +0100

    undo changes at dependency and added libs in .sh file

--- wheelmap/libs/add_jars_to_local_repository.sh
+++ wheelmap/libs/add_jars_to_local_repository.sh
@@ -2,3 +2,8 @@ mvn install:install-file -Dfile=libGoogleAnalytics.jar -DgroupId=org.wheelmap.an
     -DartifactId=googleanalytics -Dversion=1.0-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=mapsforge-map-0.2.4.jar -DgroupId=org.wheelmap.android \
     -DartifactId=mapsforge-map -Dversion=0.2.4 -Dpackaging=jar
+mvn install:install-file -Dfile=gson-2.2.4.jar -DgroupId=org.wheelmap.android \
+    -DartifactId=gson -Dversion=2.2.4 -Dpackaging=jar
+mvn install:install-file -Dfile=universal-image-loader-1.9.1.jar -DgroupId=org.wheelmap.android \
+    -DartifactId=universal-image-loader -Dversion=1.9.1 -Dpackaging=jar
+
