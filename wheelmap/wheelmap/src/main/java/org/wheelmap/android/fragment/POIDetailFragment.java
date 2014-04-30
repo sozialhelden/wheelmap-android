8edd4d87a26d63c880ae307c142e5f8ea4905e3b
Alexandra Fehkührer <alexandra.fehkuehrer@smartmobilefactory.com>
2014-04-30 17:20:47 +0200

    fixed center problem on detail screen

--- wheelmap/wheelmap/src/main/java/org/wheelmap/android/fragment/POIDetailFragment.java
+++ wheelmap/wheelmap/src/main/java/org/wheelmap/android/fragment/POIDetailFragment.java
@@ -581,8 +581,8 @@ public class POIDetailFragment extends Fragment implements
                     org.osmdroid.util.GeoPoint geoPoint = new org.osmdroid.util.GeoPoint(mCrrLatitude,
                             mCrrLongitude);
 
-                    if (mMapView != null && !isCentered) {
-                        centerMap(geoPoint, false);
+                    if (mMapView != null) {
+                        centerMap(geoPoint, true);
                     }
                 }
             });
@@ -717,7 +717,7 @@ public class POIDetailFragment extends Fragment implements
         org.osmdroid.util.GeoPoint geoPoint = new org.osmdroid.util.GeoPoint(mLocation.getLatitude(),
                 mLocation.getLongitude());
 
-        if (mMapView != null && !isCentered) {
+        if (mMapView != null) {
             centerMap(geoPoint, false);
         }
 
@@ -901,8 +901,8 @@ public class POIDetailFragment extends Fragment implements
                 org.osmdroid.util.GeoPoint geoPoint = new org.osmdroid.util.GeoPoint(mCrrLatitude,
                         mCrrLongitude);
 
-                if (mMapView != null && !isCentered) {
-                    centerMap(geoPoint, false);
+                if (mMapView != null) {
+                    centerMap(geoPoint, true);
                 }
             }
 
@@ -1370,7 +1370,7 @@ public class POIDetailFragment extends Fragment implements
     private void centerMap(org.osmdroid.util.GeoPoint geoPoint, boolean force) {
         Log.d(TAG, "centerMap: force = " + force + " isCentered = "
                 + isCentered + " geoPoint = " + geoPoint);
-        if (force || !isCentered) {
+        if (force) {
             setCenterWithOffset(geoPoint);
         }
     }
