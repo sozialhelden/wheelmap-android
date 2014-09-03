6fb156012539bc3b7469e88ca1bdff4c98bc7024
RR <rutton.r@gmail.com>
2014-09-03 10:39:57 +0200

    removed shell script

--- wheelmap/wheelmap/build_sign_production.sh
+++
@@ -1,2 +0,0 @@
-export ANDROID_HOME=/Users/torstenlemm/Documents/android-sdk-macosx
-mvn clean package  -Pproduction -DskipTests=true -Palternative-signature -Dsign.keystore=/Users/torstenlemm/Downloads/Wheelmap_2_Android/sozialhelden.keystore -Dsign.alias=sozialhelden -Dsign.storepass='IrkZun0L$pL0M$zX' -Dsign.keypass='TgM9Lfr*m79uBs.e' -e
