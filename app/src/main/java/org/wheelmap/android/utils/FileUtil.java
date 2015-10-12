0fa639833382b4d64de029d3344c371061453144
Tim Freiheit <timfreiheit@hotmail.de>
2015-10-12 17:46:02 +0200

    removed unused imports

--- app/src/main/java/org/wheelmap/android/utils/FileUtil.java
+++ app/src/main/java/org/wheelmap/android/utils/FileUtil.java
@@ -1,17 +1,12 @@
 package org.wheelmap.android.utils;
 
-import org.omg.CORBA.Environment;
-
 import android.annotation.SuppressLint;
 import android.content.ContentUris;
 import android.content.Context;
-import android.database.*;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
-
-import java.awt.*;
 
 public class FileUtil {
 
