fc3de674fda02a17593011f900a18938daf3ee84
Tim Freiheit <timfreiheit@hotmail.de>
2016-10-06 18:11:28 +0200

    dummy impl of measurement api

--- app/src/main/java/org/wheelmap/android/net/WheelmapRestService.java
+++ app/src/main/java/org/wheelmap/android/net/WheelmapRestService.java
@@ -1,9 +1,11 @@
 package org.wheelmap.android.net;
 
 import org.wheelmap.android.model.api.ApiResponse;
+import org.wheelmap.android.model.api.MeasurementMetaData;
 
 import okhttp3.MultipartBody;
 import retrofit2.Response;
+import retrofit2.http.Body;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -15,5 +17,12 @@ interface WheelmapRestService {
     @Multipart
     @POST("api/nodes/{wmId}/photos")
     Observable<Response<ApiResponse>> uploadImage(@Path("wmId") long wmId, @Part MultipartBody.Part file);
+
+    @Multipart
+    @POST("api/nodes/{wmId}/measurements")
+    Observable<Response<ApiResponse>> uploadMeasurementImage(@Path("wmId") long wmId, @Part MultipartBody.Part file);
+
+    @POST("/api/nodes/{wmId}/measurements/{measurement_id}/metadata")
+    Observable<Response<ApiResponse>> uploadMeasurementMetaData(@Path("wmId") long wmId, @Path("measurement_id") long measurementId, @Body MeasurementMetaData metaData);
 
 }
