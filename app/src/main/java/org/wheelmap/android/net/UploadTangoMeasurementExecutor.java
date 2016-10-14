1cd06fa471add2f6162a60e225a8390a9b286d63
Tim Freiheit <timfreiheit@hotmail.de>
2016-10-14 16:29:16 +0200

    use measurement api

--- app/src/main/java/org/wheelmap/android/net/UploadTangoMeasurementExecutor.java
+++ app/src/main/java/org/wheelmap/android/net/UploadTangoMeasurementExecutor.java
@@ -28,7 +28,7 @@ public class UploadTangoMeasurementExecutor {
     /**
      * temporary upload measurements over the photos api
      */
-    private static boolean USE_PHOTO_API = true;
+    private static boolean USE_PHOTO_API = false;
 
     private long wmId;
     private MeasurementInfo measurementInfo;
